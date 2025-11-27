8595368890a097a9515028d73f74e55368dd4fd8
Hassanzorkot004 <Hassan.ZORKOT@esprit.tn>
2025-11-27 01:12:44 +0000

    Refactor database connection to use environment variables
    Updated PostgreSQL connection configuration to use environment variables for sensitive data.

--- pilates-project/backend/server.js
+++ pilates-project/backend/server.js
@@ -57,13 +57,22 @@ app.use(cors());
 app.use(express.json());
 
 // Connexion PostgreSQL
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "loginsection",
+//   password: "hassan2004",
+//   port: 5432,
+// });
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "loginsection",
-  password: "hassan2004",
-  port: 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }
 });
+
 
 // ✅ Test route
 app.get("/", (req, res) => {
