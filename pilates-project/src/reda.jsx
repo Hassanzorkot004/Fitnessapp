5e56f4371f86f3bb430d28bb32909300670d5aa3
Hassanzorkot004 <Hassan.ZORKOT@esprit.tn>
2025-11-27 23:24:49 +0000

    Update reda.jsx

--- pilates-project/src/reda.jsx
+++ pilates-project/src/reda.jsx
@@ -441,7 +441,7 @@ const handleSubmit = async (e) => {
       }
 
       // ✅ APPEL REGISTER - PostgreSQL
-      const response = await fetch("http://localhost:5000/register", {
+      const response = await fetch("https://fitnessappbackend-7zbf.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -471,7 +471,7 @@ const handleSubmit = async (e) => {
       }
 
       // ✅ APPEL LOGIN - PostgreSQL
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://fitnessappbackend-7zbf.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
